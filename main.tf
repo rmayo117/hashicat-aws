fa59789497bd3d7bb12cc95ba9ea40556d7db234
Robert Mayo <rmayo117@gmail.com>
2020-10-23 21:48:35 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -122,7 +122,10 @@ resource "aws_instance" "hashicat" {
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
-  department = "devops"
+  mandatory_tags = [
+  "Department",
+  "Billable",
+]
   tags = {
     Name = "${var.prefix}-hashicat-instance"
   }
